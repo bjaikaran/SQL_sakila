4a68dae497c5e0ad10fb0d887974bb7e4cf50c1a
Bobby <bobbyjaikaran@gwu.edu>
2018-04-25 16:34:39 -0400

    Update SQL_7d.sql

--- SQL-No_07/SQL_7d.sql
+++ SQL-No_07/SQL_7d.sql
@@ -10,4 +10,4 @@ WHERE
     	c.category_id = fc.category_id 
 	AND f.film_id = fc.film_id 
     AND c.category_id IN (2, 3, 8)	-- 2: Animation Films | 3: Children Films | 8: Family Films
-	AND f.rating IN ('G', 'PG')	-- after reviewing the first dataset in the above categories, the descriptions of some of the films didn't seem too family friendly.;
+	AND f.rating IN ('G', 'PG')	-- after reviewing the first dataset in the above categories, the descriptions of some of the films didn't seem too family friendly.; 
