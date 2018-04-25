dc4033c462421f10308e86792792ae572333b0fd
Bobby <bobbyjaikaran@gwu.edu>
2018-04-25 16:40:57 -0400

    Update SQL_3a.sql

--- SQL-No_03/SQL_3a.sql
+++ SQL-No_03/SQL_3a.sql
@@ -4,7 +4,7 @@ DROP PROCEDURE IF EXISTS middle_name_Check;
 		BEGIN
 		    DECLARE CONTINUE HANDLER FOR SQLEXCEPTION BEGIN END;
 			ALTER TABLE 
-			sakila.actor 
+				sakila.actor 
 			ADD middle_name VARCHAR(45) AFTER first_name;
 		END //
 	DELIMITER ;
