3c258e02dda0b18819e9df4c4bfda40af8ac739a
Bobby <bobbyjaikaran@gwu.edu>
2018-04-25 14:05:51 -0400

    Update SQL_6b.sql

--- SQL-No_06/SQL_6b.sql
+++ SQL-No_06/SQL_6b.sql
@@ -1,12 +1,12 @@
 SELECT
-    first_name,
-    last_name,
-    SUM(amount)
+    first_name as 'first name',
+    last_name as 'last name',
+    SUM(amount) as 'August 2005 Sales'
 FROM
     sakila.staff,
     sakila.payment
 WHERE
-    	staff.staff_id = payment.staff_id 
+    		staff.staff_id = payment.staff_id 
 	AND	payment.payment_date LIKE '2005-08%'
 GROUP BY
-    payment.staff_id
+    payment.staff_id;
