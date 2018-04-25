93c9a90f2f640b7bf72ce1d68c4524facc1d2fc5
Bobby <bobbyjaikaran@gwu.edu>
2018-04-25 13:28:47 -0400

    Update SQL_3c.sql

--- SQL-No_03/SQL_3c.sql
+++ SQL-No_03/SQL_3c.sql
@@ -1,4 +1,13 @@
-ALTER TABLE
-    sakila.actor
-DROP
-    middle_name
+DROP PROCEDURE IF EXISTS `middle_name_Drop`;
+DELIMITER //
+    CREATE PROCEDURE `middle_name_Check`()
+        BEGIN
+            DECLARE CONTINUE HANDLER FOR SQLEXCEPTION BEGIN END;
+	            ALTER TABLE
+                    sakila.actor
+                DROP middle_name;
+END //
+
+DELIMITER ;
+CALL `middle_name_Drop`();
+DROP PROCEDURE `middle_name_Drop`;
