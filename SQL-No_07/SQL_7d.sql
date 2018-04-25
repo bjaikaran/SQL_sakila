4b6395f76626cf5d3ba3d7ef50e6eb777519ef99
Bobby <bobbyjaikaran@gwu.edu>
2018-04-25 15:56:27 -0400

    Update SQL_7d.sql

--- SQL-No_07/SQL_7d.sql
+++ SQL-No_07/SQL_7d.sql
@@ -1,5 +1,6 @@
 SELECT
-    title AS 'Family Friendly Movies',
+    title as 'Family Friendly Movies',
+    rating as 'Film Rating',
     description as 'Film Description'
 FROM
     sakila.film f,
@@ -8,5 +9,5 @@ FROM
 WHERE
     	c.category_id = fc.category_id 
 	AND f.film_id = fc.film_id 
-    AND c.category_id IN (2, 3, 8)  -- 2: Animation Films | 3: Children Films | 8: Family Films
-	AND f.rating IN ('G', 'PG')		-- after reviewing the first dataset in the above categories, the descriptions of some of the films didn't seem too family friendly.
+    AND c.category_id IN (2, 3, 8)	-- 2: Animation Films | 3: Children Films | 8: Family Films
+	AND f.rating IN ('G', 'PG')	-- after reviewing the first dataset in the above categories, the descriptions of some of the films didn't seem too family friendly.;
