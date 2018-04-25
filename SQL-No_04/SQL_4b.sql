82abf8cba8f5b02a1defeeddb83bb8d0a3b82541
Bobby <bobbyjaikaran@gwu.edu>
2018-04-25 13:51:10 -0400

    Update SQL_4b.sql

--- SQL-No_04/SQL_4b.sql
+++ SQL-No_04/SQL_4b.sql
@@ -1,13 +1,11 @@
 SELECT
-    a.last_name,
-    COUNT(a.last_name) AS lastNamecount
+    last_name,
+    COUNT(last_name) AS lastNamecount
 FROM
-    sakila.actor a
+    sakila.actor 
 WHERE
-    (
-        SELECT COUNT(a.last_name) AS lastNamecount FROM sakila.actor a
-    ) > 1
+    (SELECT COUNT(last_name) AS lastNamecount FROM sakila.actor) > 1
 GROUP BY
-    a.last_name
+    last_name
 ORDER BY
-    a.lastNameCount DESC;
+    lastNameCount DESC;
