9128987dc0487ef270fc90212ccaad97b9565d8f
Bobby <bobbyjaikaran@gwu.edu>
2018-04-26 10:52:58 -0400

    Update SQL_4b.sql

--- SQL-No_04/SQL_4b.sql
+++ SQL-No_04/SQL_4b.sql
@@ -3,9 +3,7 @@ SELECT
     COUNT(last_name) as lastNamecount
 FROM
     sakila.actor 
-WHERE
-    (SELECT COUNT(last_name) as lastNamecount FROM sakila.actor) > 1
 GROUP BY
     last_name
-ORDER BY
-    lastNameCount DESC;
+HAVING
+	COUNT(last_name) >1;
