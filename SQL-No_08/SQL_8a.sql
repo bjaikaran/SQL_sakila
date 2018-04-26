88ecaadda25e363767106718f49022966cddc05b
Bobby <bobbyjaikaran@gwu.edu>
2018-04-26 12:07:01 -0400

    Update SQL_8a.sql

--- SQL-No_08/SQL_8a.sql
+++ SQL-No_08/SQL_8a.sql
@@ -1,20 +1,25 @@
-CREATE OR ALTER VIEW top_5_by_genre AS (
+CREATE VIEW top_5_by_genre as
                 SELECT
-                        name AS 'Genre', 
-                        SUM(amount) AS 'Gross'
-                FROM
-                    sakila.film f,
-                    sakila.category c,
-                    sakila.film_category fc,
-                    sakila.inventory i,
-                    sakila.payment p,
-                    sakila.rental r
-                WHERE
-                        c.category_id = fc.category_id
-                    AND f.film_id = fc.film_id
-                    AND i.film_id = f.film_id
-                    AND i.inventory_id = r.inventory_id
-                    AND r.rental_id = p.rental_id
-                GROUP BY name
-                ORDER BY SUM(amount) DESC
-                LIMIT 5 )
+                      c.name as 'Genre', 
+                      SUM(p.amount) as 'Gross Revenue'
+                  FROM
+                      sakila.film f,
+                      sakila.category c,
+                      sakila.film_category fc,
+                      sakila.inventory i,
+                      sakila.payment p,
+                      sakila.rental r
+                  WHERE
+                      c.category_id = fc.category_id
+                      AND f.film_id = fc.film_id
+                      AND i.film_id = f.film_id
+                      AND i.inventory_id = r.inventory_id
+                      AND r.rental_id = p.rental_id
+                  GROUP BY name
+                  ORDER BY SUM(p.amount) DESC
+                  LIMIT 5;
+ 
+ SELECT 
+ 	*
+ FROM
+ 	top_5_by_genre;
